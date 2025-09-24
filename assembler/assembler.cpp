e2f03c9541d09d82992a743c0fa0cc3a39f23351
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-09-24 15:32:29 +0300

    add file reading logic to assembler

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -1,7 +1,28 @@
 #include "code.hpp"
+#include "constants.cpp"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int main() {
-  constexpr int num_registers = 16;
-  auto sym_table = initialize_symbol_table(num_registers);
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cerr << "Incorrect CLI arguments! Usage: " << argv[0] << " <filename>"
+         << endl;
+    return 1;
+  }
+
+  string filename = argv[1];
+  ifstream file(filename);
+
+  if (!file.is_open()) {
+    cerr << "Failed to open file: " << filename << endl;
+  }
+
+  auto sym_table = initialize_symbol_table(NUM_REGISTERS);
+
+  int variable_idx = NUM_REGISTERS;
+
   return 0;
 }
