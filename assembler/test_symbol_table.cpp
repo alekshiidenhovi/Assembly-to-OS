2f20d2cb5da6dbdbb6c9bf1fac69c9cfa3dde95d
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-10-05 12:23:56 +0300

    Replace code tests with symbol table tests

---
+++ assembler/test_symbol_table.cpp
@@ -0,0 +1,30 @@
+#include "SymbolTable.hpp"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  constexpr int NUM_REGISTERS = 16;
+  auto symbol_table = SymbolTable(NUM_REGISTERS);
+
+  assert(symbol_table.contains(Symbol("R0")));
+  assert(symbol_table.contains(Symbol("R15")));
+  assert(symbol_table.contains(Symbol("SP")));
+  assert(!symbol_table.contains(Symbol("R16")));
+
+  assert(symbol_table.getAddress(Symbol("R0")) == 0);
+  assert(symbol_table.getAddress(Symbol("R15")) == 15);
+  assert(symbol_table.getAddress(Symbol("SP")) == 0);
+  cout << "Table initialization tests passed!\n";
+
+  string symbol_name = "NEW_SYMBOL";
+  Symbol symbol(symbol_name);
+  assert(!symbol_table.contains(symbol));
+  symbol_table.addEntry(symbol, 100);
+  assert(symbol_table.contains(symbol));
+  cout << "Symbol table insertion tests passed!\n";
+
+  cout << "✅ All SymbolTable tests passed successfully!\n";
+  return 0;
+}
