d236386abe06f0eb9e18a030c2dd7bf0997a4dad
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-10-13 09:45:59 +0300

    Small main file bug fixes

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <vector>
 
+#include "Constants.hpp"
 #include "Parser.hpp"
 #include "SymbolTable.hpp"
 
@@ -18,15 +19,19 @@ int main(int argc, char* argv[]) {
   const std::filesystem::path src_filepath(argv[1]);
   const std::string src_filename = src_filepath.stem().string();
   const std::string src_extension = src_filepath.extension().string();
-  if (src_extension.ends_with(".asm")) {
+  if (!src_extension.ends_with(hack_assembler::kAssemblyFileExtension)) {
     std::cerr << "The source file needs to be an assembly file, you passed: "
               << src_extension << std::endl;
+    return 1;
   }
-  const std::filesystem::path out_filepath(src_filename + ".hack");
+  const std::filesystem::path out_filepath(
+      src_filename + std::string(hack_assembler::kHackFileExtension));
   std::ifstream src_file(src_filepath);
 
   if (!src_file.is_open()) {
-    std::cerr << "Failed to open file: " << src_filepath << std::endl;
+    std::cerr << "Failed to open Hack assembly file: " << src_filepath
+              << std::endl;
+    return 1;
   }
 
   // Initialize the symbol table
@@ -36,7 +41,7 @@ int main(int argc, char* argv[]) {
       {Symbol("ARG"), 2},         {Symbol("THIS"), 3},
       {Symbol("THAT"), 4},        {Symbol("SCREEN"), 16384},
       {Symbol("KEYBOARD"), 24576}};
-  for (auto& [symbol, address] : predefinedSymbols) {
+  for (const auto& [symbol, address] : predefinedSymbols) {
     symbolTable.addEntry(symbol, address);
   }
   for (int registerNumber = 0;
