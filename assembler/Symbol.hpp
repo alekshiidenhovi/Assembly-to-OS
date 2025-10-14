71f4dccd20279d870081b284b28eb3b3ff89fadb
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-10-14 21:15:06 +0300

    move Symbol methods to header file and remove Symbol TU

--- assembler/Symbol.hpp
+++ assembler/Symbol.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <functional>
 #include <ostream>
+#include <regex>
 #include <string>
+#include <utility>
 
 namespace hack_assembler {
 class Symbol {
@@ -9,27 +11,35 @@ class Symbol {
   /**
    * Single argument constructor
    */
-  explicit Symbol(std::string name);
+  explicit Symbol(std::string name) : name_(std::move(name)) {
+    if (!isValid(name_)) {
+      throw std::invalid_argument("Invalid Hack symbol: " + name_);
+    }
+  }
 
   /**
    * String conversion operator
    */
-  operator const std::string&() const noexcept;
+  operator const std::string&() const noexcept { return name_; }
 
   /**
    * String conversion method
    */
-  const std::string& str() const noexcept;
+  const std::string& str() const noexcept { return name_; }
 
   /**
    * Symbol equality operator
    */
-  bool operator==(const Symbol& other) const noexcept;
+  bool operator==(const Symbol& other) const noexcept {
+    return name_ == other.name_;
+  }
 
   /**
    * Symbol inequality operator
    */
-  bool operator!=(const Symbol& other) const noexcept;
+  bool operator!=(const Symbol& other) const noexcept {
+    return !(*this == other);
+  }
 
   /**
    * Symbol validation method. Returns true if the symbol adheres to Hack
@@ -40,9 +50,13 @@ class Symbol {
    * signs, and colon.
    *   - cannot start with a digit.
    */
-  static bool isValid(const std::string& s);
-
-  friend std::ostream& operator<<(std::ostream& os, const Symbol& symbol);
+  static bool isValid(const std::string& s) {
+    static const std::regex pattern(R"(^[A-Za-z_\.\$\:][A-Za-z0-9_\.\$\:]*$)");
+    return regex_match(s, pattern);
+  }
+  friend std::ostream& operator<<(std::ostream& os, const Symbol& symbol) {
+    return os << symbol.str();
+  }
 
  private:
   const std::string name_;
