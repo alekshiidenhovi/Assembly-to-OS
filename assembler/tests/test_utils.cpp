2ba1fc6196864d6abdbdba2bbdc748fecfa955f6
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-10-20 21:18:52 +0300

    fix utils test

--- assembler/tests/test_utils.cpp
+++ assembler/tests/test_utils.cpp
@@ -3,8 +3,8 @@
 #include "Utils.hpp"
 
 TEST(UtilsTest, DecimalTo16Bits) {
-  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(0), "0000'0000'0000'0000");
-  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(1), "0000'0000'0000'0001");
-  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(10), "0000'0000'0000'1010");
-  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(100), "0000'0000'0110'0100");
+  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(0), "0000000000000000");
+  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(1), "0000000000000001");
+  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(10), "0000000000001010");
+  EXPECT_EQ(hack_assembler::utils::decimalTo16Bits(100), "0000000001100100");
 }
